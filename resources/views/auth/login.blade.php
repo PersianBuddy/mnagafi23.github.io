a401968cb0d070ba9c12f35a522a2e52c7de28ce
mnagafi23 <mnajafi0014@users.noreply.github.com>
2018-02-09 10:45:47 +0330

    Fix persian language align right to left

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,16 +5,16 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
+                <div class="panel-heading">ورود کاربران</div>
 
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">ایمیل</label>
+                            
 
-                            <div class="col-md-6">
+                            <div class="col-md-5 col-md-offset-2">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
@@ -23,12 +23,13 @@
                                     </span>
                                 @endif
                             </div>
+                            <label for="email" class="col-md-3 control-label">ایمیل</label>
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">رمز عبور</label>
+                            
 
-                            <div class="col-md-6">
+                            <div class="col-md-5 col-md-offset-2">
                                 <input id="password" type="password" class="form-control" name="password" required>
 
                                 @if ($errors->has('password'))
@@ -37,27 +38,29 @@
                                     </span>
                                 @endif
                             </div>
+                            <label for="password" class="col-md-3 control-label">رمز عبور</label>
                         </div>
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <div class="checkbox">
                                     <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> مرا بخاطر بسپار
                                     </label>
                                 </div>
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
+                            <div class="col-md-6 col-md-offset-4">
+                                
                                 <button type="submit" class="btn btn-primary">
-                                    Login
+                                    ورود
                                 </button>
-
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
+                                    رمز عبور را فراموش کرده ام.
                                 </a>
+                                
                             </div>
                         </div>
                     </form>
